ad871be15b7314c03c6cae432935d91eb4f76b22
a <a>
2024-12-05 15:21:40 -0800

    java_georgia

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleSpicey(){
+    for (const el of document.querySelectorAll(".spicey")){
+        el.hidden=!el.hidden;
+    }
+}
+function toggleSweet(){
+    for (const el of document.querySelectorAll(".sweet")){
+        el.hidden=!el.hidden;
+        }
+}
+function toggleeVegetarian(){
+    for (const el of document.querySelectorAll(".vegetarian")){
+        el.hidden=!el.hidden;
+        }
+}
+function toggleFruity(){
+    for (const el of document.querySelectorAll(".fruity")){
+        el.hidden=!el.hidden;
+        }
+}
+function toggleVegan(){
+    for (const el of document.querySelectorAll(".vegan")){
+    el.hidden=!el.hidden;
+    }
+}
+
+
